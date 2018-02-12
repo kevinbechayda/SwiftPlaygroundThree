ea1c4c5dc275c43d92b63fb20e875f106aac1aab
Kevin Bechayda <kevinbechayda@gmail.com>
2018-02-12 10:19:47 -1000

    Declared an array of type String. Initialized an array using an array literal. Printed the array. Printed one index from array.

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -3,3 +3,35 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//how do we declare a variable with an array
+
+var fruitBasket: [String]
+
+//simple string variable
+
+var simpleString: String
+
+//intialize an array using asn Array Literal
+
+fruitBasket = ["Bananas", "Apples", "Watermelons"]
+
+//print fruitBasket
+
+    print(fruitBasket)
+
+//for-in loop
+
+for name in fruitBasket {
+//print "I like to eat" before every name in fruitBasket
+print("I like to eat " + name)
+}
+
+//lets print out item at index 1
+
+print(fruitBasket[1])
+
+//lets print out item at index 0
+
+print(fruitBasket[0])
+
